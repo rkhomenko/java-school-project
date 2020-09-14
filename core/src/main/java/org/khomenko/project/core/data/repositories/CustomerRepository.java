683e233c60071da816bffad49b0d1fde594b0bae
Roman Khomenko <romankhomenko1995@gmail.com>
2020-09-14 12:58:14 +0300

    Added method to repository

--- core/src/main/java/org/khomenko/project/core/data/repositories/CustomerRepository.java
+++ core/src/main/java/org/khomenko/project/core/data/repositories/CustomerRepository.java
@@ -3,5 +3,8 @@ package org.khomenko.project.core.data.repositories;
 import org.khomenko.project.core.data.models.Customer;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
 }
